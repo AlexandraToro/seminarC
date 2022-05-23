4f5b2b1051e0696db0d4b797939f87881f91867a
«Alexandra» <sashatoropova@gmail.com>
2022-05-23 14:10:52 +0700

    add task027

---
+++ Task027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите любое натуральное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumNum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр числа {num} равна {(GetSumNum(num))}");
