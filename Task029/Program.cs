d8c347402aa3c65b5fdaab8c871054209d1832ca
«Alexandra» <sashatoropova@gmail.com>
2022-05-23 14:11:00 +0700

    add task029

---
+++ Task029/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая задаёт массив целых чисел и выводит их на экран.
+//Размер массива определяется пользователем.
+
+Console.WriteLine("Введите количество элементов массива: ");
+int num = int.Parse(Console.ReadLine());
+int[] array = new int[num];
+
+int[] GetArray(int[] arr1)
+{
+    int i = 0;
+    while (i < arr1.Length)
+    {
+        arr1[i] = new Random().Next(0, 100);
+        i++;
+    }
+    return arr1;
+}
+void PrintArray(int[] arr2)
+{
+    for (int count = 0; count < arr2.Length; count++)
+    {
+        Console.Write($"{arr2[count]} ");
+    }
+}
+GetArray(array);
+PrintArray(array);
